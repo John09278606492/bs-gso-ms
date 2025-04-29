0beec0c654a44663c8602f4b3fa10b47416934e0
John09278606492 <fronteras135@gmail.com>
2025-04-29 14:09:14 +0800

    new file:   bootstrap/cache/blade-icons.php 	new file:   bootstrap/cache/filament/panels/admin.php

---
+++ bootstrap/cache/filament/panels/admin.php
@@ -0,0 +1,131 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'app.filament.resources.collection-enrollment-resource.pages.create-collection-enrollment' => 'App\\Filament\\Resources\\CollectionEnrollmentResource\\Pages\\CreateCollectionEnrollment',
+    'app.filament.resources.collection-enrollment-resource.pages.edit-collection-enrollment' => 'App\\Filament\\Resources\\CollectionEnrollmentResource\\Pages\\EditCollectionEnrollment',
+    'app.filament.resources.collection-enrollment-resource.pages.list-collection-enrollments' => 'App\\Filament\\Resources\\CollectionEnrollmentResource\\Pages\\ListCollectionEnrollments',
+    'app.filament.resources.collection-enrollment-resource.widgets.collection-records-widget' => 'App\\Filament\\Resources\\CollectionEnrollmentResource\\Widgets\\CollectionRecordsWidget',
+    'app.filament.resources.college-resource.pages.create-college' => 'App\\Filament\\Resources\\CollegeResource\\Pages\\CreateCollege',
+    'app.filament.resources.college-resource.pages.edit-college' => 'App\\Filament\\Resources\\CollegeResource\\Pages\\EditCollege',
+    'app.filament.resources.college-resource.pages.list-colleges' => 'App\\Filament\\Resources\\CollegeResource\\Pages\\ListColleges',
+    'app.filament.resources.college-resource.relation-managers.programs-relation-manager' => 'App\\Filament\\Resources\\CollegeResource\\RelationManagers\\ProgramsRelationManager',
+    'app.filament.resources.enrollment-resource.pages.create-enrollment' => 'App\\Filament\\Resources\\EnrollmentResource\\Pages\\CreateEnrollment',
+    'app.filament.resources.enrollment-resource.pages.edit-enrollment' => 'App\\Filament\\Resources\\EnrollmentResource\\Pages\\EditEnrollment',
+    'app.filament.resources.enrollment-resource.pages.invoice' => 'App\\Filament\\Resources\\EnrollmentResource\\Pages\\Invoice',
+    'app.filament.resources.enrollment-resource.pages.list-enrollments' => 'App\\Filament\\Resources\\EnrollmentResource\\Pages\\ListEnrollments',
+    'app.filament.resources.enrollment-resource.relation-managers.pays-relation-manager' => 'App\\Filament\\Resources\\EnrollmentResource\\RelationManagers\\PaysRelationManager',
+    'app.filament.resources.enrollment-resource.widgets.total-pay-widget' => 'App\\Filament\\Resources\\EnrollmentResource\\Widgets\\TotalPayWidget',
+    'app.filament.resources.enrollment-resource.widgets.total-payable-widget' => 'App\\Filament\\Resources\\EnrollmentResource\\Widgets\\TotalPayableWidget',
+    'app.filament.resources.pay-resource.pages.create-pay' => 'App\\Filament\\Resources\\PayResource\\Pages\\CreatePay',
+    'app.filament.resources.pay-resource.pages.edit-pay' => 'App\\Filament\\Resources\\PayResource\\Pages\\EditPay',
+    'app.filament.resources.pay-resource.pages.list-pays' => 'App\\Filament\\Resources\\PayResource\\Pages\\ListPays',
+    'app.filament.resources.pay-resource.widgets.pays-widget' => 'App\\Filament\\Resources\\PayResource\\Widgets\\PaysWidget',
+    'app.filament.resources.program-resource.pages.create-program' => 'App\\Filament\\Resources\\ProgramResource\\Pages\\CreateProgram',
+    'app.filament.resources.program-resource.pages.edit-program' => 'App\\Filament\\Resources\\ProgramResource\\Pages\\EditProgram',
+    'app.filament.resources.program-resource.pages.list-programs' => 'App\\Filament\\Resources\\ProgramResource\\Pages\\ListPrograms',
+    'app.filament.resources.program-resource.relation-managers.yearlevels-relation-manager' => 'App\\Filament\\Resources\\ProgramResource\\RelationManagers\\YearlevelsRelationManager',
+    'app.filament.resources.schoolyear-resource.pages.create-schoolyear' => 'App\\Filament\\Resources\\SchoolyearResource\\Pages\\CreateSchoolyear',
+    'app.filament.resources.schoolyear-resource.pages.edit-schoolyear' => 'App\\Filament\\Resources\\SchoolyearResource\\Pages\\EditSchoolyear',
+    'app.filament.resources.schoolyear-resource.pages.list-schoolyears' => 'App\\Filament\\Resources\\SchoolyearResource\\Pages\\ListSchoolyears',
+    'app.filament.resources.schoolyear-resource.relation-managers.semesters-relation-manager' => 'App\\Filament\\Resources\\SchoolyearResource\\RelationManagers\\SemestersRelationManager',
+    'app.filament.resources.stud-resource.pages.create-stud' => 'App\\Filament\\Resources\\StudResource\\Pages\\CreateStud',
+    'app.filament.resources.stud-resource.pages.edit-stud' => 'App\\Filament\\Resources\\StudResource\\Pages\\EditStud',
+    'app.filament.resources.stud-resource.pages.list-studs' => 'App\\Filament\\Resources\\StudResource\\Pages\\ListStuds',
+    'app.filament.resources.stud-resource.relation-managers.enrollments-relation-manager' => 'App\\Filament\\Resources\\StudResource\\RelationManagers\\EnrollmentsRelationManager',
+    'app.filament.resources.stud-resource.relation-managers.sibling-relation-manager' => 'App\\Filament\\Resources\\StudResource\\RelationManagers\\SiblingRelationManager',
+    'app.filament.resources.stud-resource.widgets.total-widget' => 'App\\Filament\\Resources\\StudResource\\Widgets\\TotalWidget',
+    'app.filament.resources.student-resource.pages.create-student' => 'App\\Filament\\Resources\\StudentResource\\Pages\\CreateStudent',
+    'app.filament.resources.student-resource.pages.edit-student' => 'App\\Filament\\Resources\\StudentResource\\Pages\\EditStudent',
+    'app.filament.resources.student-resource.pages.list-students' => 'App\\Filament\\Resources\\StudentResource\\Pages\\ListStudents',
+    'app.filament.resources.student-resource.relation-managers.payments-relation-manager' => 'App\\Filament\\Resources\\StudentResource\\RelationManagers\\PaymentsRelationManager',
+    'app.filament.resources.user-resource.pages.create-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\CreateUser',
+    'app.filament.resources.user-resource.pages.edit-user' => 'App\\Filament\\Resources\\UserResource\\Pages\\EditUser',
+    'app.filament.resources.user-resource.pages.list-users' => 'App\\Filament\\Resources\\UserResource\\Pages\\ListUsers',
+    'app.filament.resources.yearlevel-resource.pages.create-yearlevel' => 'App\\Filament\\Resources\\YearlevelResource\\Pages\\CreateYearlevel',
+    'app.filament.resources.yearlevel-resource.pages.edit-yearlevel' => 'App\\Filament\\Resources\\YearlevelResource\\Pages\\EditYearlevel',
+    'app.filament.resources.yearlevel-resource.pages.list-yearlevels' => 'App\\Filament\\Resources\\YearlevelResource\\Pages\\ListYearlevels',
+    'app.filament.resources.yearlevel-resource.relation-managers.yearlevelpayments-relation-manager' => 'App\\Filament\\Resources\\YearlevelResource\\RelationManagers\\YearlevelpaymentsRelationManager',
+    'app.filament.pages.auth.login' => 'App\\Filament\\Pages\\Auth\\Login',
+    'app.filament.pages.dashboard' => 'App\\Filament\\Pages\\Dashboard',
+    'app.filament.pages.student-information' => 'App\\Filament\\Pages\\StudentInformation',
+    'app.filament.widgets.admin-widget' => 'App\\Filament\\Widgets\\AdminWidget',
+    'app.filament.widgets.big-stats' => 'App\\Filament\\Widgets\\BigStats',
+    'app.filament.widgets.caudit-widget' => 'App\\Filament\\Widgets\\CauditWidget',
+    'app.filament.widgets.college-widget' => 'App\\Filament\\Widgets\\CollegeWidget',
+    'app.filament.widgets.program-widget' => 'App\\Filament\\Widgets\\ProgramWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.pages.auth.edit-profile' => 'Filament\\Pages\\Auth\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.pages.auth.email-verification.email-verification-prompt' => 'Filament\\Pages\\Auth\\EmailVerification\\EmailVerificationPrompt',
+    'filament.pages.auth.password-reset.request-password-reset' => 'Filament\\Pages\\Auth\\PasswordReset\\RequestPasswordReset',
+    'filament.pages.auth.password-reset.reset-password' => 'Filament\\Pages\\Auth\\PasswordReset\\ResetPassword',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Pages\\Dashboard.php' => 'App\\Filament\\Pages\\Dashboard',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Pages\\StudentInformation.php' => 'App\\Filament\\Pages\\StudentInformation',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => 'C:\\laragon\\www\\public_html - Copy\\app\\Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\CollectionEnrollmentResource.php' => 'App\\Filament\\Resources\\CollectionEnrollmentResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\CollegeResource.php' => 'App\\Filament\\Resources\\CollegeResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\EnrollmentResource.php' => 'App\\Filament\\Resources\\EnrollmentResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\PayResource.php' => 'App\\Filament\\Resources\\PayResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\ProgramResource.php' => 'App\\Filament\\Resources\\ProgramResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\SchoolyearResource.php' => 'App\\Filament\\Resources\\SchoolyearResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\StudResource.php' => 'App\\Filament\\Resources\\StudResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\StudentResource.php' => 'App\\Filament\\Resources\\StudentResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\UserResource.php' => 'App\\Filament\\Resources\\UserResource',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Resources\\YearlevelResource.php' => 'App\\Filament\\Resources\\YearlevelResource',
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => 'C:\\laragon\\www\\public_html - Copy\\app\\Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Widgets\\AdminWidget.php' => 'App\\Filament\\Widgets\\AdminWidget',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Widgets\\BigStats.php' => 'App\\Filament\\Widgets\\BigStats',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Widgets\\CauditWidget.php' => 'App\\Filament\\Widgets\\CauditWidget',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Widgets\\CollegeWidget.php' => 'App\\Filament\\Widgets\\CollegeWidget',
+    'C:\\laragon\\www\\public_html - Copy\\app\\Filament\\Widgets\\ProgramWidget.php' => 'App\\Filament\\Widgets\\ProgramWidget',
+    0 => 'App\\Filament\\Widgets\\AdminWidget',
+    1 => 'App\\Filament\\Widgets\\BigStats',
+    2 => 'App\\Filament\\Widgets\\CauditWidget',
+    3 => 'App\\Filament\\Widgets\\CollegeWidget',
+    4 => 'App\\Filament\\Widgets\\ProgramWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => 'C:\\laragon\\www\\public_html - Copy\\app\\Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
